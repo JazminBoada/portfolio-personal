771e55080979357987468355527a2c6ea756b7e4
Jazmin Boada <boadajazmin@gmail.com>
2025-02-25 14:27:04 -0300

    change aboutme

--- src/components/layouts/Aboutme.jsx
+++ src/components/layouts/Aboutme.jsx
@@ -7,16 +7,17 @@ function Aboutme() {
         <h1 className="text-xl md:text-2xl font-semibold mb-4">Sobre mi</h1>
         <section className="border border-zinc-700 bg-lightBlack p-4 rounded-md">
           <p className="pb-2">
-            Hace dos años comencé a formarme de manera autodidacta para
+            Hace más de dos años comencé a formarme de manera autodidacta para
             especializarme como
             <b> desarrolladora web Frontend</b>.
           </p>
           <p>
-            Me especializo en diseñar interfaces atractivas y funcionales que
-            mejoran la experiencia del usuario. También me enfoco en desarrollar
+            Llevo a cabo el diseño interfaces atractivas y funcionales que
+            mejoran la experiencia del usuario. Me enfoco en desarrollar
             soluciones innovadoras y fáciles de usar. Soy una persona
-            comprometida, con habilidades para el trabajo en equipo y dispuesta
-            siempre a nuevos desafíos.
+            comprometida, con habilidades para el trabajo en equipo. Busco
+            colaborar con empresas o emprendedores para transformar sus ideas en
+            productos digitales efectivos.
           </p>
         </section>
       </main>
